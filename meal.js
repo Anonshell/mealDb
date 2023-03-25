63a6926d1b0c0279a34cfcc52dc1d3fdba617c79
Ammar <ammaraslam7164@gmail.com>
2023-03-25 20:40:56 +0600

    2nd

--- meal.js
+++ meal.js
@@ -27,7 +27,10 @@ const displayMeals = (meals) => {
              <div class="card-body">
                <h5 class="card-title">${meal.strMeal}</h5>
                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-             </div>
+               <button onclick="loadMealDetail(${meal.idMeal}) type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#mealDetails">
+             Details
+             </button>
+               </div>
            </div>
       
            `;
@@ -48,18 +51,28 @@ const searchMeals = () => {
 
        loadMeals(searchText);
 
+}
+
+
+const loadMealDetail= idMeal =>{
+
+
+       console.log(idMeal)
+
+      const url = `www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
+          
+      console.log(url)
+
+         fetch(url)
+         .then(res => res.json())
+         .then(data => console.log(data));
 
 
 
-
-         
-
-         
 
 }
 
 
 
-
-loadMeals();
+loadMeals('pasta');
  

40cc8249dfacaee8cae82583e52f7b69cb658cc4
Ammar <ammaraslam7164@gmail.com>
2023-03-25 14:14:00 +0600

    first

---
+++ countries.js
@@ -0,0 +1,88 @@
+const loadCountries = () => {
+  fetch("https://restcountries.com/v3.1/all")
+    .then((res) => res.json())
+    .then((data) => displayCountries(data));
+};
+
+const displayCountries = (countries) => {
+  const countriesContainer = document.getElementById("all-countries");
+
+  //  for(const country of countries)
+  //  {
+  //         console.log(country);
+
+  //  }
+
+  countries.forEach((country) => {
+    console.log(country);
+
+    const div = document.createElement("div");
+
+    div.classList.add("country");
+
+    div.innerHTML = `
+    
+    
+
+
+             <h3> Name : ${country.name.common} </h3>
+           <p> Capital : ${
+             country.capital ? country.capital[0] : "No Capital"
+           } </p>
+           
+           <button onclick= "loadCountryDetails('${
+             country.cca2
+           }')">Details </button>
+        
+    
+    `;
+
+    countriesContainer.appendChild(div);
+  });
+};
+
+const loadCountryDetails = (code) => {
+
+    const url =` https://restcountries.com/v3.1/alpha/${code}`
+ 
+
+                          console.log(url)
+                          fetch(url)
+                          .then(res => res.json())   
+                          .then(data => console.log(data))
+
+
+
+
+}
+
+
+
+
+
+const showCountryDetails = country =>{
+
+    console.log(country)
+
+    const detailContainer = document.getElementById('country-detail')
+
+    detailContainer.innerHTML = `
+      
+    
+             <h3> Name: ${country.name.common} </h3>
+
+             <img src=" ${country.flags.png}">
+          
+    
+    
+    
+    
+    
+    
+    
+    
+    `
+
+}
+
+loadCountries();
